9e38800b59a87f9b780679d4c88a1da85926f427
Fedaz <federico.diaz.ilm+hub@gmail.com>
2022-04-13 19:32:23 -0300

    Se adjuntan archivos c#
    Se suben los archivos del proyecto

---
+++ BaseCalculadora.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpCalculadora
+{
+    internal class BaseCalculadora
+    {
+
+        public BaseCalculadora() { }
+
+        public int sumar(int a,int b)
+        {
+            return a + b;
+        }
+        public int restar(int a, int b)
+        {
+            if (a >= b)
+            {
+                return a - b;
+            }
+            else {
+                return b - a;
+            }
+        }
+        public int multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int dividir(int a, int b)
+        {
+            try
+            {
+                if (a >= b)
+                {
+                    return a / b;
+                } else
+                {
+                    return b / a;
+                }
+            } catch (Exception)
+            {
+                Console.WriteLine("No es posibile dividir por 0");
+                return -1;
+            }
+        }
+
+
+    }
+}
---
+++ code.cs
@@ -0,0 +1,27 @@
+﻿using TpCalculadora;
+
+public class Code {
+
+
+    public static int getRandomNumber() {
+        Random random = new Random();
+        return random.Next(1,50);
+    }
+
+    static void Main()
+    {
+        CalculadoraEstandar calculadora = new CalculadoraEstandar();
+        for (int i = 1; i <= 10; i++){
+            int number1 = getRandomNumber();
+            int number2 = getRandomNumber();
+            Console.WriteLine($"Operación número: {i}°");
+            Console.WriteLine($"La suma de {number1} y {number2} da {calculadora.sumar(number1, number2)}");
+            Console.WriteLine("");
+            Console.WriteLine($"La resta de {number1} y {number2} da {calculadora.restar(number1, number2)}");
+            Console.WriteLine("");
+            Console.WriteLine($"La multiplicación de {number1} y {number2} da {calculadora.multiplicar(number1, number2)}");
+        }
+        
+        
+    }
+}
